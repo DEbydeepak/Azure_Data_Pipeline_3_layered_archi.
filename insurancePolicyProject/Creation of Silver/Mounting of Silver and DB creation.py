dc7e974a83386ac8bedba9203f393a5ee549db3e
Rajasekhar4314 <82395124+Rajasekhar4314@users.noreply.github.com>
2023-10-01 21:01:39 +0530

    Update Mounting of Silver and DB creation.py

--- insurancePolicyProject/Creation of Silver/Mounting of Silver and DB creation.py
+++ insurancePolicyProject/Creation of Silver/Mounting of Silver and DB creation.py
@@ -6,7 +6,7 @@
 # COMMAND ----------
 
 dbutils.fs.mount( source = 'wasbs://silver@missionadestgacc468.blob.core.windows.net', 
-                 mount_point= '/mnt/silver', extra_configs ={'fs.azure.sas.silver.missionadestgacc468.blob.core.windows.net':'?sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupyx&se=2023-10-05T10:45:40Z&st=2023-09-26T02:45:40Z&spr=https&sig=slcQVJwm11vgxOA%2F%2Bxl41HFzgLPaH%2FlSMtBmotx8vNU%3D'})
+                 mount_point= '/mnt/silver', extra_configs ={'fs.azure.sas.silver.missionadestgacc468.blob.core.windows.net':'?sv=2022-11-02&ss=bfqt&se=2023-10-05T10:45:40Z&st=2023-09-26T02:45:40Z&spr=https&sig=slcQVJwm11vgxOA%2F%2Bxl41HFzgLPaH%2FlSMtBmotx8vNU%3D'})
 
 # COMMAND ----------
 
